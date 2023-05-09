f7e0591582bb6ec2ab8cb6dbe931a624199f5b03
Dimitris Grammatikogiannis <dg@dgrammatiko.dev>
2023-05-09 14:53:38 +0200

    redo

--- src/plugins/task/json2cf/services/provider.php
+++ src/plugins/task/json2cf/services/provider.php
@@ -1,22 +1,23 @@
 <?php
 
 /**
- * @copyright   Mark
- * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ * (C) 2023 Dimitrios Grammatikogiannis
+ * GNU General Public License version 3 or later
  */
 
-defined('_JEXEC') or die;
+defined('_JEXEC') || die;
 
 use Joomla\CMS\Extension\PluginInterface;
 use Joomla\CMS\Factory;
 use Joomla\CMS\Plugin\PluginHelper;
+use Joomla\Database\DatabaseInterface;
 use Joomla\DI\Container;
 use Joomla\DI\ServiceProviderInterface;
 use Joomla\Event\DispatcherInterface;
-use Joomla\Plugin\Task\Json2CF\Extension\Json2CF;
-use Joomla\Utilities\ArrayHelper;
+use Dgrammatiko\Plugin\Task\Json2cf\Extension\Json2cf;
 
-return new class () implements ServiceProviderInterface {
+return new class () implements ServiceProviderInterface
+{
     /**
      * Registers the service provider with a DI container.
      *
@@ -24,21 +25,20 @@ return new class () implements ServiceProviderInterface {
      *
      * @return  void
      *
-     * @since   4.2.0
+     * @since   0.0.1
      */
     public function register(Container $container)
     {
-
         $container->set(
             PluginInterface::class,
             function (Container $container) {
                 $dispatcher = $container->get(DispatcherInterface::class);
-
-                $plugin = new Json2CF(
+                $plugin     = new Json2cf(
                     $dispatcher,
                     (array) PluginHelper::getPlugin('task', 'json2cf')
                 );
                 $plugin->setApplication(Factory::getApplication());
+                $plugin->setDatabase($container->get(DatabaseInterface::class));
 
                 return $plugin;
             }
